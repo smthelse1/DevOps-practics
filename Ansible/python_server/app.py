7b98699dbeba7074de4c69c9667a7a811a5a5311
17teen <117625042+smthelse1@users.noreply.github.com>
2025-08-22 14:59:29 +0700

    Add files via upload

---
+++ Ansible/python_server/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return SimpleHTTPRequestHandler.do_GET(self)
+
+def run(server_class=HTTPServer, handler_class=MyHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Server running on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
